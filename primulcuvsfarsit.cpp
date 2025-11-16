34d509e4cbf8713843b0529ff865986d9df456f0
Laurentiur6789 <99979705+Laurentiur6789@users.noreply.github.com>
2025-11-16 18:16:13 +0200

    primulcuvsfarsit.cpp

--- primulcuvsfarsit.cpp
+++ primulcuvsfarsit.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+
 using namespace std;
 
 int main()
@@ -13,14 +14,11 @@ int main()
     for (int i=2; i<=n; i++)
     {
         cin.getline(s,21);
-    }
-    for (int i=2; i<=n; ++i)
-    {
-      if(strcmp(s+strlen(s)-strlen(c),c)!=0)
+
+      if(strcmp(s+strlen(s)-strlen(c),c)==0)
         {
             nr++;
         }
     }
     cout<<nr;
 }
-//mama ei de problema
